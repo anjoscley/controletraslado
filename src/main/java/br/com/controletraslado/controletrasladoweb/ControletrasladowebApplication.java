286bc310437418a94c9e7b9f3ef67cc451c1f0a1
Dariclei Anjos <anjoscley@gmail.com>
2022-06-26 23:37:02 -0300

    Commit Inicial

---
+++ src/main/java/br/com/controletraslado/controletrasladoweb/ControletrasladowebApplication.java
@@ -0,0 +1,13 @@
+package br.com.controletraslado.controletrasladoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControletrasladowebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControletrasladowebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/controletraslado/controletrasladoweb/ControletrasladowebApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.controletraslado.controletrasladoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControletrasladowebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
